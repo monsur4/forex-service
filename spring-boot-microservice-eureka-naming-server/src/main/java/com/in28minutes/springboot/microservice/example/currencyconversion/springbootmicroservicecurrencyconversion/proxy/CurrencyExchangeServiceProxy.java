3813d66fffdef670a427083a45dbc6e1b065f40e
monsuru.okuniyi <okuniyimonsuru@yahoo.com>
2022-06-15 15:25:44 +0100

    add eureka for client registration

--- spring-boot-microservice-eureka-naming-server/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/proxy/CurrencyExchangeServiceProxy.java
+++ spring-boot-microservice-eureka-naming-server/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/proxy/CurrencyExchangeServiceProxy.java
@@ -1,6 +1,5 @@
 package com.in28minutes.springboot.microservice.example.currencyconversion.springbootmicroservicecurrencyconversion.proxy;
 
-import com.in28minutes.springboot.microservice.example.currencyconversion.springbootmicroservicecurrencyconversion.config.LoadBalancerConfiguration;
 import com.in28minutes.springboot.microservice.example.currencyconversion.springbootmicroservicecurrencyconversion.model.CurrencyConversionBean;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name="forex-service")
-@LoadBalancerClient(name="forex-service", configuration = LoadBalancerConfiguration.class)
+@LoadBalancerClient(name="forex-service")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
+    CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 }
